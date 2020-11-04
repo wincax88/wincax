ee6a521915366851cdffe1a3424f27bc4391eba7
michael <wincax@gmail.com>
2020-11-04 10:30:33 +0800

    init

---
+++ precache-manifest.b9c8884b2f3c0f052b4f44a1ab2b8a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be56ce460729db5ca710649f341f496e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3ed0c11d88cccbb699d",
+    "url": "./static/css/2.ce016b7d.chunk.css"
+  },
+  {
+    "revision": "10daa4c0de5406cde80d",
+    "url": "./static/css/main.cb413274.chunk.css"
+  },
+  {
+    "revision": "d3ed0c11d88cccbb699d",
+    "url": "./static/js/2.a3f7889c.chunk.js"
+  },
+  {
+    "revision": "5865bead3734c60a432deff05e011b58",
+    "url": "./static/js/2.a3f7889c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10daa4c0de5406cde80d",
+    "url": "./static/js/main.28f63ab8.chunk.js"
+  },
+  {
+    "revision": "dac0d41fd532f7145944",
+    "url": "./static/js/runtime-main.1dad88fe.js"
+  }
+]);
